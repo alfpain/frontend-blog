0dafd34f0fdb6e700b2c7b6bcad2a318e2714c10
brandon <josueknd@hotmail.es>
2014-11-14 14:38:55 -0600

    veriosn beta

--- js/script.js
+++ js/script.js
@@ -1,33 +1,158 @@
 // Javascript Code.
 $(document).ready(function(){
+
     $('#texto').click(function() {
         $(this).fadeOut(10);
+        $("#texto2").fadeOut(10);
+        $("#texto3").fadeOut(10);
     });
     $("#texto").click(function(){
-        $("#post1").fadeIn("fast")
-        $(".aco").fadeIn("fast")
-        $("#comentario").load("_posts/2014-11-09-cognits-frontend-blog.md");
+        $("#post1").fadeIn("fast") /* este va ser que aparesca mi div principal decis vos*/
+        $(".aco").fadeIn("fast") /*este es ek div donde esta guardado mi post y mi comentario */
+        $("#comentario").load("_posts/2014-11-09-cognits-frontend-blog.md"); /* y este ese es el post donde lo carga*/
     });
     /*ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss*/
     $("button").click(function() {
         $("#post1").fadeOut(10);
-        $(".aco").fadeOut("fast")
+        $(".aco").fadeOut("fast");
+
 
     });
     $(".blog").click(function() {
      $("#post1").fadeOut(10);
     $(".aco").fadeOut("fast")
 
+
+    });
+    $(".home").click(function() {
+     $("#post1").fadeOut(10);
+    $(".aco").fadeOut("fast")
+
+
     });
 
     $("button").click(function(){
         $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
     });
     $(".blog").click(function(){
         $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
     });
 
+    $(".home").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
+    });
+
+    /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+ $('#texto2').click(function() {
+        $(this).fadeOut(10);
+        $("#texto").fadeOut(10);
+        $("#texto3").fadeOut(10);
+    });
+
+$("#texto2").click(function(){
+        $("#post12").fadeIn("fast")
+        $(".aco2").fadeIn("fast")
+        $("#comentario2").load("_posts/post.md");
+    });
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+    $(".but2").click(function() {
+        $("#post12").fadeOut(10);
+        $(".aco2").fadeOut("fast");
+      });
+
+  $(".home").click(function() {
+     $("#post12").fadeOut(10);
+    $(".aco2").fadeOut("fast")
+    });
+
+
+    $(".blog").click(function() {
+     $("#post12").fadeOut(10);
+    $(".aco2").fadeOut("fast")
+
+
+    });
+
+    $(".but2").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn(10);
+    });
+    $(".blog").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn(10);
+    });
+
+    $(".home").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
+    });
+
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+ $('#texto3').click(function() {
+        $(this).fadeOut(10);
+        $("#texto").fadeOut(10);
+        $("#texto2").fadeOut(10);
+        $("#texto3").fadeOut(10);
+    });
+
+$("#texto3").click(function(){
+        $("#post13").fadeIn("fast")
+        $(".aco3").fadeIn("fast")
+        $("#comentario3").load("_posts/post.md");
+    });
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+    $(".but3").click(function() {
+        $("#post13").fadeOut(10);
+        $(".aco3").fadeOut("fast");
+
+});
+    $(".home").click(function() {
+     $("#post13").fadeOut(10);
+    $(".aco3").fadeOut("fast")
+
+
+
+
+    });
+    $(".blog").click(function() {
+     $("#post13").fadeOut(10);
+    $(".aco3").fadeOut("fast")
+
+
+    });
+
+    $(".but3").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
+    });
+    $(".blog").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
+    });
+    $(".home").click(function(){
+        $("#texto").fadeIn("fast")
+        $("#texto2").fadeIn("fast")
+        $("#texto3").fadeIn("fast");
+    });
+
+
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
     $(".button1").click(function() {
+
         var m = new Date();
 var dateString =
   m.getUTCFullYear() +"/"+
@@ -41,30 +166,43 @@ var dateString =
         $("input").val("");
         $(".list").append("<div class='item'>" +"Usuario: "+ toAdd +"<br/>"+"comment: "+toAdd2+ "<br/>"+ dateString+"</div>" );
         }),
-        $(document).on("click",".item", function(){
-            $(this).remove()
-            });
-    
 
-
-    $(".button2").click(function() {
-               var m2 = new Date();
-var dateString2 =
-  m2.getUTCFullYear() +"/"+
-  ("0" + (m2.getUTCMonth()+1)).slice(-2) +"/"+
-  ("0" + m2.getUTCDate()).slice(-2) + " " +
-  ("0" + m2.getUTCHours()).slice(-2) + ":" +
-  ("0" + m2.getUTCMinutes()).slice(-2) + ":" +
-  ("0" + m2.getUTCSeconds()).slice(-2);
-
-        var toAdd4 = $("input[name=checkListItem4]").val();
-        var toAdd3 = $("input[name=checkListItem3]").val();
+    $("#but").click(function() {
+        var m = new Date();
+var dateString =  m.getUTCFullYear() +"/"+
+  ("0" + (m.getUTCMonth()+1)).slice(-2) +"/"+
+  ("0" + m.getUTCDate()).slice(-2) + " " +
+  ("0" + m.getUTCHours()).slice(-2) + ":" +
+  ("0" + m.getUTCMinutes()).slice(-2) + ":" +
+  ("0" + m.getUTCSeconds()).slice(-2);
+        var toAdd2 = $("input[name=checkListItem4]").val();
+        var toAdd = $("input[name=checkListItem3]").val();
         $("input").val("");
-        $(".list2").append("<div class='item'>" +"Usuario: "+ toAdd3 +"<br/>"+"comment: "+toAdd4+ "<br/>"+ dateString2+"</div>" );
-        }),
-        $(document).on("click",".item", function(){
-            $(this).remove()
-            });
+        $(".list2").append("<div class='item'>" +"Usuario: "+ toAdd +"<br/>"+"comment: "+toAdd2+ "<br/>"+ dateString+"</div>" );
+        });
 
+    $("#but2").click(function() {
+        var m = new Date();
+var dateString =  m.getUTCFullYear() +"/"+
+  ("0" + (m.getUTCMonth()+1)).slice(-2) +"/"+
+  ("0" + m.getUTCDate()).slice(-2) + " " +
+  ("0" + m.getUTCHours()).slice(-2) + ":" +
+  ("0" + m.getUTCMinutes()).slice(-2) + ":" +
+  ("0" + m.getUTCSeconds()).slice(-2);
+        var toAdd2 = $("input[name=checkListItem6]").val();
+        var toAdd = $("input[name=checkListItem5]").val();
+        $("input").val("");
+        $(".list3").append("<div class='item'>" +"Usuario: "+ toAdd +"<br/>"+"comment: "+toAdd2+ "<br/>"+ dateString+"</div>" );
+        });
 
+$(".buscar").click(function(){
+
+  buscar = prompt("to search?");
+  if (buscar == toAdd) {
+    confirm(buscar)
+  }
+  else {
+  confirm("no encontrado");
+  }
+})
     });
